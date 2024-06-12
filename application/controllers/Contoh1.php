0a913f48a55d03e713a287a2612b1ed8f3f17cc2
alponsiyus2003 <alponsiyus281103@gmail.com>
2024-06-12 18:29:06 +0700

    Tugas 01

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Alpon Siyus
+ Saya tinggal di daerah Pontianak
+ Saya sangat menyukai belajar bahasa pemrograman";
+ }
+}
