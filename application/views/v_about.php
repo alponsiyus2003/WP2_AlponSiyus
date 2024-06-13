a608fafa5a3c35b1cae54f7cc7e137e04b4e5a75
alponsiyus2003 <alponsiyus281103@gmail.com>
2024-06-13 17:08:28 +0700

    Pertemuan 03 dan Tugas 02

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Alpon</li>
+		<li>Nama Belakang : Siyus</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="circle">
+		<li> Gang Keluarga</li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Pontianak</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Badminton</li>
+		<li>Jogging</li>
+	</ul>
+</section>
