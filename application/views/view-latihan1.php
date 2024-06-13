9ec1976d1b0fe631cbeec7e5c1d683b384525233
alponsiyus2003 <alponsiyus281103@gmail.com>
2024-06-13 16:03:12 +0700

    Pertemuan ke 3 Dan Tugas 2

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Nama : Alpon Siyus <br>
+    NIM : 15220580 <br>
+    Kelas : 15.4C.30 <br>
+    <hr>
+    Mari kita melakukan penjumlah di pemogramaan ini<br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu 
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
